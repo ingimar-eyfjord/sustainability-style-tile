0ad6df3a56fbd00a5394adc3772af640447219df
ingimar <ingimareys93@gmail.com>
2019-09-12 23:12:11 +0200

    Got the clients to append as well as the information about them, I have also created some logos for identifying them, like vegan logos and such and display them in display block if it's true, as they are by defult display none. What we need is a short description for all the clients. I have not sorted them by catagory yet, but I have made a seporate HTML file for restourants. But we could just do a simple filter on on the INDEX if we want.

--- js.js
+++ js.js
@@ -17,6 +17,44 @@ function showName(nameData){
 	const template = document.querySelector("template").content;
 	const copy = template.cloneNode(true);
 	console.log(nameData)
-	copy.querySelector("h1").textContent=nameData.gsx$category.$t;
-	document.querySelector("body").appendChild(copy);
+	copy.querySelector(".clientname").textContent=nameData.gsx$name.$t;
+	copy.querySelector(".clientlogo img").src = `media/clientslogos/${nameData.gsx$imgname.$t}.jpg`;
+	
+	function vegan() {
+    if (nameData.gsx$vegan.$t === "1") {
+		copy.querySelector(".vegan").style.display = "block";
+//      copy.querySelector(".vegan img").src = `media/icons/vegan.png`;
+    }
+  }
+  vegan()
+	
+	function zeroWaste() {
+    if (nameData.gsx$zerowaste.$t === "1") {
+		copy.querySelector(".zeroWaste").style.display = "block";
+//      copy.querySelector(".vegan img").src = `media/icons/vegan.png`;
+    }
+  }
+  zeroWaste()
+		function organic() {
+    if (nameData.gsx$organic.$t === "1") {
+		copy.querySelector(".organic").style.display = "block";
+//      copy.querySelector(".vegan img").src = `media/icons/vegan.png`;
+    }
+  }
+  organic()
+		function raw() {
+    if (nameData.gsx$raw.$t === "1") {
+		copy.querySelector(".raw").style.display = "block";
+//      copy.querySelector(".vegan img").src = `media/icons/vegan.png`;
+    }
+  }
+  raw()
+		function NonPolluting() {
+    if (nameData.gsx$nonpolluting.$t === "1") {
+		copy.querySelector(".NonPolluting").style.display = "block";
+//      copy.querySelector(".vegan img").src = `media/icons/vegan.png`;
+    }
+  }
+  NonPolluting()
+	document.querySelector("article.showclientcards").appendChild(copy);
 }
